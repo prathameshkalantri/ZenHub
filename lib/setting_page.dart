69d1da5a7cc8983022fb936ce1f45a325f1e0cea
prathameshkalantri <kalantriprathamesh@gmail.com>
2024-04-29 20:43:51 -0700

    Fetch username

--- lib/setting_page.dart
+++ lib/setting_page.dart
@@ -1,3 +1,4 @@
+//
 // import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:firebase_storage/firebase_storage.dart';
 // import 'package:flutter/material.dart';
@@ -20,6 +21,26 @@
 //   XFile? _image;
 //   String? newPassword;
 //   String? confirmPassword;
+//   String? _profilePhotoURL; // Add this variable to store the profile photo URL
+//   String? _username;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _getProfilePhotoURL(); // Call the method to fetch profile photo URL when the page initializes
+//   }
+//
+//   // Method to fetch the profile photo URL from Firestore
+//   void _getProfilePhotoURL() async {
+//     User? user = FirebaseAuth.instance.currentUser;
+//     if (user != null) {
+//       CollectionReference usersRef = FirebaseFirestore.instance.collection('users');
+//       DocumentSnapshot snapshot = await usersRef.doc(user.uid).get();
+//       setState(() {
+//         _profilePhotoURL = snapshot['profile_photo_url'];
+//       });
+//     }
+//   }
 //
 //   Future<void> _getImage(ImageSource source) async {
 //     final picker = ImagePicker();
@@ -31,36 +52,6 @@
 //       });
 //     }
 //   }
-//
-//   // Future<void> _showImagePicker(BuildContext context) async {
-//   //   showModalBottomSheet(
-//   //     context: context,
-//   //     builder: (BuildContext context) {
-//   //       return SafeArea(
-//   //         child: Wrap(
-//   //           children: <Widget>[
-//   //             ListTile(
-//   //               leading: Icon(Icons.camera),
-//   //               title: Text('Camera'),
-//   //               onTap: () {
-//   //                 _getImage(ImageSource.camera);
-//   //                 Navigator.pop(context);
-//   //               },
-//   //             ),
-//   //             ListTile(
-//   //               leading: Icon(Icons.photo_library),
-//   //               title: Text('Gallery'),
-//   //               onTap: () {
-//   //                 _getImage(ImageSource.gallery);
-//   //                 Navigator.pop(context);
-//   //               },
-//   //             ),
-//   //           ],
-//   //         ),
-//   //       );
-//   //     },
-//   //   );
-//   // }
 //
 //   void _showImagePicker(BuildContext context) async {
 //     final picker = ImagePicker();
@@ -118,6 +109,11 @@
 //           // Update the 'profile_photo_url' field in the user document
 //           await userDocRef.update({'profile_photo_url': downloadURL});
 //
+//           // Update the background image URL
+//           setState(() {
+//             _profilePhotoURL = downloadURL;
+//           });
+//
 //           // Show a message indicating the image was uploaded successfully
 //           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
 //             content: Text('Image uploaded successfully'),
@@ -137,7 +133,20 @@
 //     }
 //   }
 //
-//
+//   Future<String?> _fetchUsername() async {
+//     User? user = FirebaseAuth.instance.currentUser;
+//     if (user != null) {
+//       try {
+//         // Fetch username from Firestore
+//         DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+//         return userDoc['username'];
+//       } catch (e) {
+//         print('Error fetching username: $e');
+//         return null;
+//       }
+//     }
+//     return null;
+//   }
 //   // Function to handle changing the password
 //   void _changePassword() async {
 //     String? currentPassword = '';
@@ -240,6 +249,11 @@
 //                 radius: 50,
 //                 backgroundImage: FileImage(File(_image!.path)),
 //               )
+//                   : _profilePhotoURL != null
+//                   ? CircleAvatar(
+//                 radius: 50,
+//                 backgroundImage: NetworkImage(_profilePhotoURL!),
+//               )
 //                   : CircleAvatar(
 //                 radius: 50,
 //                 backgroundImage: AssetImage('assets/image2.jpg'),
@@ -253,7 +267,12 @@
 //           )
 //               : Column(
 //             children: [
-//               CircleAvatar(
+//               _profilePhotoURL != null
+//                   ? CircleAvatar(
+//                 radius: 50,
+//                 backgroundImage: NetworkImage(_profilePhotoURL!),
+//               )
+//                   : CircleAvatar(
 //                 radius: 50,
 //                 backgroundImage: AssetImage('assets/image2.jpg'),
 //               ),
@@ -341,11 +360,6 @@
 //     );
 //   }
 // }
-//
-//   //   Navigator.pushReplacementNamed(context, '/signin');
-//   // }
-//   //
-
 
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -371,11 +385,13 @@ class _SettingPageState extends State<SettingPage> {
   String? newPassword;
   String? confirmPassword;
   String? _profilePhotoURL; // Add this variable to store the profile photo URL
+  String? _username; // Add this variable to store the fetched username
 
   @override
   void initState() {
     super.initState();
-    _getProfilePhotoURL(); // Call the method to fetch profile photo URL when the page initializes
+    _getProfilePhotoURL();
+    _fetchUsername(); // Call the method to fetch the username when the page initializes
   }
 
   // Method to fetch the profile photo URL from Firestore
@@ -390,6 +406,22 @@ class _SettingPageState extends State<SettingPage> {
     }
   }
 
+  // Method to fetch the username from Firestore
+  void _fetchUsername() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      try {
+        // Fetch username from Firestore
+        DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+        setState(() {
+          _username = userDoc['username'];
+        });
+      } catch (e) {
+        print('Error fetching username: $e');
+      }
+    }
+  }
+
   Future<void> _getImage(ImageSource source) async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: source);
@@ -401,80 +433,6 @@ class _SettingPageState extends State<SettingPage> {
     }
   }
 
-  // void _showImagePicker(BuildContext context) async {
-  //   final picker = ImagePicker();
-  //   final pickedFile = await showModalBottomSheet<XFile>(
-  //     context: context,
-  //     builder: (BuildContext context) {
-  //       return Wrap(
-  //         children: [
-  //           ListTile(
-  //             leading: Icon(Icons.camera),
-  //             title: Text('Take a photo'),
-  //             onTap: () async {
-  //               Navigator.pop(context, await picker.pickImage(source: ImageSource.camera));
-  //             },
-  //           ),
-  //           ListTile(
-  //             leading: Icon(Icons.photo_library),
-  //             title: Text('Choose from gallery'),
-  //             onTap: () async {
-  //               Navigator.pop(context, await picker.pickImage(source: ImageSource.gallery));
-  //             },
-  //           ),
-  //         ],
-  //       );
-  //     },
-  //   );
-  //
-  //   if (pickedFile != null) {
-  //     setState(() {
-  //       _image = pickedFile;
-  //     });
-  //
-  //     try {
-  //       File imageFile = File(pickedFile.path);
-  //       String fileName = 'profile_image_${DateTime.now().millisecondsSinceEpoch}.jpg';
-  //
-  //       // Create a reference to the location you want to upload to in Firebase Storage
-  //       final Reference storageRef = FirebaseStorage.instance.ref().child('profile_images/$fileName');
-  //
-  //       // Upload the file to Firebase Storage
-  //       UploadTask uploadTask = storageRef.putFile(imageFile);
-  //
-  //       // Get the download URL of the uploaded image
-  //       TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() {});
-  //
-  //       String downloadURL = await taskSnapshot.ref.getDownloadURL();
-  //
-  //       // Update the user's profile photo URL in Firestore
-  //       User? user = FirebaseAuth.instance.currentUser;
-  //       if (user != null) {
-  //         // Assuming you have a Firestore collection named 'users' and a document for each user
-  //         CollectionReference usersRef = FirebaseFirestore.instance.collection('users');
-  //         DocumentReference userDocRef = usersRef.doc(user.uid);
-  //
-  //         // Update the 'profile_photo_url' field in the user document
-  //         await userDocRef.update({'profile_photo_url': downloadURL});
-  //
-  //         // Show a message indicating the image was uploaded successfully
-  //         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-  //           content: Text('Image uploaded successfully'),
-  //         ));
-  //       } else {
-  //         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-  //           content: Text('User not signed in'),
-  //         ));
-  //       }
-  //     } catch (e) {
-  //       // Handle any errors that occur during the upload process
-  //       print('Error uploading image: $e');
-  //       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-  //         content: Text('Error uploading image'),
-  //       ));
-  //     }
-  //   }
-  // }
   void _showImagePicker(BuildContext context) async {
     final picker = ImagePicker();
     final pickedFile = await showModalBottomSheet<XFile>(
@@ -554,7 +512,6 @@ class _SettingPageState extends State<SettingPage> {
       }
     }
   }
-
 
   // Function to handle changing the password
   void _changePassword() async {
@@ -686,10 +643,12 @@ class _SettingPageState extends State<SettingPage> {
                 backgroundImage: AssetImage('assets/image2.jpg'),
               ),
               SizedBox(height: 20),
-              Text(
-                'Username',
+              _username != null // Check if username is fetched
+                  ? Text(
+                _username!, // Display the fetched username
                 style: TextStyle(fontSize: 20),
-              ),
+              )
+                  : CircularProgressIndicator(), // Show a loading indicator while fetching username
             ],
           ),
           SizedBox(height: 20),
