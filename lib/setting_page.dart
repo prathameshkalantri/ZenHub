78794cdfe94c29b486df40e2a099e9b754ba289d
prathameshkalantri <kalantriprathamesh@gmail.com>
2024-05-16 00:01:33 -0700

    Completed the Home page

--- lib/setting_page.dart
+++ lib/setting_page.dart
@@ -1,367 +1,3 @@
-//
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_storage/firebase_storage.dart';
-// import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
-// import 'dart:io';
-// import 'reminder_page.dart';
-// import 'dart:io';
-// import 'workout_page.dart'; // Import WorkoutPage
-// import 'home_page.dart'; // Import HomePage
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'location.dart';
-//
-// class SettingPage extends StatefulWidget {
-//   @override
-//   _SettingPageState createState() => _SettingPageState();
-// }
-//
-// class _SettingPageState extends State<SettingPage> {
-//   bool isEditMode = false;
-//   XFile? _image;
-//   String? newPassword;
-//   String? confirmPassword;
-//   String? _profilePhotoURL; // Add this variable to store the profile photo URL
-//   String? _username;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _getProfilePhotoURL(); // Call the method to fetch profile photo URL when the page initializes
-//   }
-//
-//   // Method to fetch the profile photo URL from Firestore
-//   void _getProfilePhotoURL() async {
-//     User? user = FirebaseAuth.instance.currentUser;
-//     if (user != null) {
-//       CollectionReference usersRef = FirebaseFirestore.instance.collection('users');
-//       DocumentSnapshot snapshot = await usersRef.doc(user.uid).get();
-//       setState(() {
-//         _profilePhotoURL = snapshot['profile_photo_url'];
-//       });
-//     }
-//   }
-//
-//   Future<void> _getImage(ImageSource source) async {
-//     final picker = ImagePicker();
-//     final pickedFile = await picker.pickImage(source: source);
-//
-//     if (pickedFile != null) {
-//       setState(() {
-//         _image = pickedFile;
-//       });
-//     }
-//   }
-//
-//   void _showImagePicker(BuildContext context) async {
-//     final picker = ImagePicker();
-//     final pickedFile = await showModalBottomSheet<XFile>(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return Wrap(
-//           children: [
-//             ListTile(
-//               leading: Icon(Icons.camera),
-//               title: Text('Take a photo'),
-//               onTap: () async {
-//                 Navigator.pop(context, await picker.pickImage(source: ImageSource.camera));
-//               },
-//             ),
-//             ListTile(
-//               leading: Icon(Icons.photo_library),
-//               title: Text('Choose from gallery'),
-//               onTap: () async {
-//                 Navigator.pop(context, await picker.pickImage(source: ImageSource.gallery));
-//               },
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//
-//     if (pickedFile != null) {
-//       setState(() {
-//         _image = pickedFile;
-//       });
-//
-//       try {
-//         File imageFile = File(pickedFile.path);
-//         String fileName = 'profile_image_${DateTime.now().millisecondsSinceEpoch}.jpg';
-//
-//         // Create a reference to the location you want to upload to in Firebase Storage
-//         final Reference storageRef = FirebaseStorage.instance.ref().child('profile_images/$fileName');
-//
-//         // Upload the file to Firebase Storage
-//         UploadTask uploadTask = storageRef.putFile(imageFile);
-//
-//         // Get the download URL of the uploaded image
-//         TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() {});
-//
-//         String downloadURL = await taskSnapshot.ref.getDownloadURL();
-//
-//         // Update the user's profile photo URL in Firestore
-//         User? user = FirebaseAuth.instance.currentUser;
-//         if (user != null) {
-//           // Assuming you have a Firestore collection named 'users' and a document for each user
-//           CollectionReference usersRef = FirebaseFirestore.instance.collection('users');
-//           DocumentReference userDocRef = usersRef.doc(user.uid);
-//
-//           // Update the 'profile_photo_url' field in the user document
-//           await userDocRef.update({'profile_photo_url': downloadURL});
-//
-//           // Update the background image URL
-//           setState(() {
-//             _profilePhotoURL = downloadURL;
-//           });
-//
-//           // Show a message indicating the image was uploaded successfully
-//           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//             content: Text('Image uploaded successfully'),
-//           ));
-//         } else {
-//           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//             content: Text('User not signed in'),
-//           ));
-//         }
-//       } catch (e) {
-//         // Handle any errors that occur during the upload process
-//         print('Error uploading image: $e');
-//         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//           content: Text('Error uploading image'),
-//         ));
-//       }
-//     }
-//   }
-//
-//   Future<String?> _fetchUsername() async {
-//     User? user = FirebaseAuth.instance.currentUser;
-//     if (user != null) {
-//       try {
-//         // Fetch username from Firestore
-//         DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
-//         return userDoc['username'];
-//       } catch (e) {
-//         print('Error fetching username: $e');
-//         return null;
-//       }
-//     }
-//     return null;
-//   }
-//   // Function to handle changing the password
-//   void _changePassword() async {
-//     String? currentPassword = '';
-//
-//     // Show dialog to input current password
-//     await showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           title: Text('Change Password'),
-//           content: Column(
-//             mainAxisSize: MainAxisSize.min,
-//             children: [
-//               TextField(
-//                 decoration: InputDecoration(labelText: 'Current Password'),
-//                 onChanged: (value) => currentPassword = value,
-//                 obscureText: true,
-//               ),
-//               TextField(
-//                 decoration: InputDecoration(labelText: 'New Password'),
-//                 onChanged: (value) => newPassword = value,
-//                 obscureText: true,
-//               ),
-//               TextField(
-//                 decoration: InputDecoration(labelText: 'Confirm New Password'),
-//                 onChanged: (value) => confirmPassword = value,
-//                 obscureText: true,
-//               ),
-//             ],
-//           ),
-//           actions: [
-//             TextButton(
-//               onPressed: () {
-//                 Navigator.pop(context);
-//               },
-//               child: Text('Cancel'),
-//             ),
-//             TextButton(
-//               onPressed: () async {
-//                 if (newPassword == confirmPassword) {
-//                   // Passwords match, update the password
-//                   // Your password update logic here
-//                 } else {
-//                   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//                     content: Text('New passwords do not match!'),
-//                   ));
-//                 }
-//                 Navigator.pop(context);
-//               },
-//               child: Text('Update'),
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//   }
-//
-//   void _logout() {
-//     // Navigate to sign-in page
-//     Navigator.pushReplacementNamed(context, '/signin');
-//   }
-//
-//   void _locateNearbyLocations() {
-//     // Navigate to a page displaying nearby locations on Google Maps
-//     Navigator.push(
-//       context,
-//       MaterialPageRoute(builder: (context) => NearbyLocationsPage()),
-//     );
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(
-//           'Settings',
-//           style: TextStyle(fontWeight: FontWeight.bold),
-//         ),
-//         automaticallyImplyLeading: false, // Hide the back button
-//         actions: [
-//           IconButton(
-//             icon: Icon(isEditMode ? Icons.done : Icons.edit),
-//             onPressed: () {
-//               setState(() {
-//                 isEditMode = !isEditMode;
-//               });
-//             },
-//           ),
-//         ],
-//       ),
-//       body: Column(
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         children: [
-//           SizedBox(height: 20),
-//           isEditMode
-//               ? Column(
-//             children: [
-//               _image != null
-//                   ? CircleAvatar(
-//                 radius: 50,
-//                 backgroundImage: FileImage(File(_image!.path)),
-//               )
-//                   : _profilePhotoURL != null
-//                   ? CircleAvatar(
-//                 radius: 50,
-//                 backgroundImage: NetworkImage(_profilePhotoURL!),
-//               )
-//                   : CircleAvatar(
-//                 radius: 50,
-//                 backgroundImage: AssetImage('assets/image2.jpg'),
-//               ),
-//               SizedBox(height: 20),
-//               ElevatedButton(
-//                 onPressed: () => _showImagePicker(context),
-//                 child: Text('Change Photo'),
-//               ),
-//             ],
-//           )
-//               : Column(
-//             children: [
-//               _profilePhotoURL != null
-//                   ? CircleAvatar(
-//                 radius: 50,
-//                 backgroundImage: NetworkImage(_profilePhotoURL!),
-//               )
-//                   : CircleAvatar(
-//                 radius: 50,
-//                 backgroundImage: AssetImage('assets/image2.jpg'),
-//               ),
-//               SizedBox(height: 20),
-//               Text(
-//                 'Username',
-//                 style: TextStyle(fontSize: 20),
-//               ),
-//             ],
-//           ),
-//           SizedBox(height: 20),
-//           // Add reminder option below the username
-//           ListTile(
-//             leading: Icon(Icons.notifications),
-//             title: Text('Reminder'),
-//             onTap: () {
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(builder: (context) => ReminderPage()),
-//               );
-//             },
-//           ),
-//           SizedBox(height: 20),
-//           // Add Change Password button
-//           ListTile(
-//             leading: Icon(Icons.lock),
-//             title: Text('Change Password'),
-//             onTap: _changePassword,
-//           ),
-//           SizedBox(height: 20),
-//           // Add Locate Nearby Locations option
-//           ListTile(
-//             leading: Icon(Icons.location_on),
-//             title: Text('Locate Nearby Locations'),
-//             onTap: _locateNearbyLocations,
-//           ),
-//           SizedBox(height: 20),
-//           ListTile(
-//             leading: Icon(Icons.logout),
-//             title: Text('Logout'),
-//             onTap: _logout,
-//           ),
-//         ],
-//       ),
-//       bottomNavigationBar: BottomAppBar(
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceAround,
-//           children: [
-//             IconButton(
-//               icon: Icon(Icons.home),
-//               onPressed: () {
-//                 Navigator.pushReplacementNamed(context, '/home');
-//               },
-//             ),
-//             IconButton(
-//               icon: Icon(Icons.fitness_center),
-//               onPressed: () {
-//                 Navigator.pushReplacementNamed(context, '/workout');
-//               },
-//             ),
-//             IconButton(
-//               icon: Icon(Icons.settings),
-//               onPressed: () {
-//                 // Do nothing since we are already on the SettingPage
-//               },
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-// // Placeholder page for displaying nearby locations
-// class NearbyLocationsPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Nearby Locations'),
-//       ),
-//       body: Center(
-//         child: Text('Display nearby locations on Google Maps here'),
-//       ),
-//     );
-//   }
-// }
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -380,6 +16,7 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
+  int _currentIndex = 2;
   bool isEditMode = false;
   XFile? _image;
   String? newPassword;
@@ -693,19 +330,28 @@ class _SettingPageState extends State<SettingPage> {
               icon: Icon(Icons.home),
               onPressed: () {
                 Navigator.pushReplacementNamed(context, '/home');
+                setState(() {
+                  _currentIndex = 0; // Update current index
+                });
               },
+              color: _currentIndex == 0 ? Colors.cyan : null, // Highlight if this is the selected item
             ),
             IconButton(
               icon: Icon(Icons.fitness_center),
               onPressed: () {
                 Navigator.pushReplacementNamed(context, '/workout');
+                setState(() {
+                  _currentIndex = 1; // Update current index
+                });
               },
+              color: _currentIndex == 1 ? Colors.cyan : null, // Highlight if this is the selected item
             ),
             IconButton(
               icon: Icon(Icons.settings),
               onPressed: () {
                 // Do nothing since we are already on the SettingPage
               },
+              color: _currentIndex == 2 ? Colors.cyan : null, // Highlight if this is the selected item
             ),
           ],
         ),
@@ -728,3 +374,5 @@ class NearbyLocationsPage extends StatelessWidget {
     );
   }
 }
+
+
--- lib/workout_page.dart
+++ lib/workout_page.dart
@@ -1,3 +1,194 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart'; // Import Services for rootBundle
+// import 'yoga_page.dart';
+// import 'pilates_page.dart';
+// import 'full_body_page.dart';
+// import 'stretching_page.dart';
+// import 'setting_page.dart'; // Import SettingPage
+// import 'home_page.dart'; // Import HomePage
+//
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   precacheImages(); // Preload and cache images
+//
+//   runApp(MyApp());
+// }
+//
+// void precacheImages() async {
+//   // Load images from assets
+//   final yogaImage = await loadAssetImage('assets/yoga.jpg');
+//   final pilatesImage = await loadAssetImage('assets/pillates.jpg');
+//   final fullBodyImage = await loadAssetImage('assets/fullbody.jpg');
+//   final stretchingImage = await loadAssetImage('assets/stretching.jpg');
+//
+//   // Create a dummy BuildContext using GlobalKey
+//   final GlobalKey dummyKey = GlobalKey();
+//   final BuildContext dummyContext = dummyKey.currentContext!;
+//
+//   // Preload and cache images using the dummy context
+//   precacheImage(yogaImage, dummyContext);
+//   precacheImage(pilatesImage, dummyContext);
+//   precacheImage(fullBodyImage, dummyContext);
+//   precacheImage(stretchingImage, dummyContext);
+// }
+//
+// Future<ImageProvider> loadAssetImage(String path) async {
+//   final ByteData data = await rootBundle.load(path);
+//   return MemoryImage(data.buffer.asUint8List());
+// }
+//
+// class ExerciseOption extends StatelessWidget {
+//   final String title;
+//   final ImageProvider image;
+//   final VoidCallback onTap;
+//
+//   const ExerciseOption({
+//     required this.title,
+//     required this.image,
+//     required this.onTap,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       color: Colors.white, // Set the color of the card to white
+//       elevation: 5, // Add elevation for shadow effect
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.circular(10),
+//       ),
+//       child: ListTile(
+//         contentPadding: EdgeInsets.symmetric(horizontal: 20),
+//         title: Row(
+//           children: [
+//             Expanded(
+//               child: Text(
+//                 title,
+//                 style: TextStyle(fontSize: 20),
+//               ),
+//             ),
+//             SizedBox(width: 10),
+//             Image(
+//               image: image,
+//               width: 160, // Adjust image width as needed
+//               height: 100, // Adjust image height as needed
+//               fit: BoxFit.cover, // Ensure the image covers the space
+//             ),
+//           ],
+//         ),
+//         onTap: onTap,
+//       ),
+//     );
+//   }
+// }
+//
+// class WorkoutPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'Workout',
+//           style: TextStyle(fontWeight: FontWeight.bold),
+//         ),
+//         automaticallyImplyLeading: false, // Remove the back button
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             SizedBox(height: 20),
+//             SizedBox(
+//               height: 120, // Adjust height as needed
+//               child: ExerciseOption(
+//                 title: 'Yoga',
+//                 image: AssetImage('assets/yoga.jpg'), // Add your image path
+//                 onTap: () {
+//                   Navigator.push(context, MaterialPageRoute(builder: (context) => YogaPage()));
+//                 },
+//               ),
+//             ),
+//             SizedBox(height: 20),
+//             SizedBox(
+//               height: 120, // Adjust height as needed
+//               child: ExerciseOption(
+//                 title: 'Pilates',
+//                 image: AssetImage('assets/pillates.jpg'), // Add your image path
+//                 onTap: () {
+//                   Navigator.push(context, MaterialPageRoute(builder: (context) => PilatesPage()));
+//                 },
+//               ),
+//             ),
+//             SizedBox(height: 20),
+//             SizedBox(
+//               height: 120, // Adjust height as needed
+//               child: ExerciseOption(
+//                 title: 'Full Body',  //full body exercise
+//                 image: AssetImage('assets/fullbody.jpg'), // Add your image path
+//                 onTap: () {
+//                   Navigator.push(context, MaterialPageRoute(builder: (context) => FullBodyPage()));
+//                 },
+//               ),
+//             ),
+//             SizedBox(height: 20),
+//             SizedBox(
+//               height: 120, // Adjust height as needed
+//               child: ExerciseOption(
+//                 title: 'Stretching',
+//                 image: AssetImage('assets/stretching.jpg'), // Add your image path
+//                 onTap: () {
+//                   Navigator.push(context, MaterialPageRoute(builder: (context) => StretchingPage()));
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       bottomNavigationBar: BottomAppBar(
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceAround,
+//           children: [
+//             IconButton(
+//               icon: Icon(Icons.home),
+//               onPressed: () {
+//                 Navigator.pushReplacementNamed(context, '/home');
+//               },
+//             ),
+//             IconButton(
+//               icon: Icon(Icons.fitness_center),
+//               onPressed: () {
+//                 // Do nothing since we are already on the WorkoutPage
+//               },
+//             ),
+//             IconButton(
+//               icon: Icon(Icons.settings),
+//               onPressed: () {
+//                 Navigator.pushReplacementNamed(context, '/setting');
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Fitness App',
+//       initialRoute: '/workout',
+//       routes: {
+//         '/workout': (context) => WorkoutPage(),
+//         '/home': (context) => HomePage(),
+//         '/setting': (context) => SettingPage(),
+//       },
+//     );
+//   }
+// }
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart'; // Import Services for rootBundle
 import 'yoga_page.dart';
@@ -81,7 +272,14 @@ class ExerciseOption extends StatelessWidget {
   }
 }
 
-class WorkoutPage extends StatelessWidget {
+class WorkoutPage extends StatefulWidget {
+  @override
+  _WorkoutPageState createState() => _WorkoutPageState();
+}
+
+class _WorkoutPageState extends State<WorkoutPage> {
+  int _currentIndex = 1; // Initially set to index 1 for WorkoutPage
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -143,30 +341,38 @@ class WorkoutPage extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/home');
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.fitness_center),
-              onPressed: () {
-                // Do nothing since we are already on the WorkoutPage
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.settings),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/setting');
-              },
-            ),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          switch (index) {
+            case 0:
+              Navigator.pushReplacementNamed(context, '/home');
+              break;
+            case 2:
+              Navigator.pushReplacementNamed(context, '/setting');
+              break;
+          }
+        },
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            activeIcon: Icon(Icons.home, color: Colors.cyan),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Workout',
+            activeIcon: Icon(Icons.fitness_center, color: Colors.cyan),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            activeIcon: Icon(Icons.settings, color: Colors.cyan),
+          ),
+        ],
       ),
     );
   }
